cfee94966df76f4e87efd2cbe487ebf249c34dd0
HanniaTsui <159938570+HanniaTsui@users.noreply.github.com>
2024-02-13 10:04:16 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola");
+		
+		JFrame ventana = new JFrame("Ejemplo");
+		ventana.setVisible(true);
+		ventana.setSize(500,300);
+		ventana.setLocation(100,100);
+		
+		
+	}
+
+}
