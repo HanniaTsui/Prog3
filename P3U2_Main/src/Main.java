49926414c7144b1a4dcc36c63e80f20e4daa1586
HanniaTsui <159938570+HanniaTsui@users.noreply.github.com>
2024-02-20 09:45:34 -0700

    Unidad 2: ejercicio 13

--- P3U2_Main/src/Main.java
+++ P3U2_Main/src/Main.java
@@ -1,6 +1,4 @@
-import java.awt.Dimension;
 
-import javax.swing.JFrame;
 
 public class Main {
 
--- P3U2_Main/src/Ventana.java
+++ P3U2_Main/src/Ventana.java
@@ -1,7 +1,10 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Ventana extends JFrame{
@@ -10,12 +13,13 @@ public class Ventana extends JFrame{
 		// TODO Auto-generated constructor stub
 		this.setVisible(true); 
 		this.setTitle("Mi ventana");
-		this.setSize(500,300);
-		this.setLocation(100,100);
+		this.setSize(920,560);
+		this.setLocation(120,120);
 		this.setLocationRelativeTo(null);
-		this.setMaximumSize(new Dimension (750,750));
+		this.setMaximumSize(new Dimension (600,600));
 		this.setMinimumSize(new Dimension (250,250));
 		//ventana.setResizable(false);
+		this.setLayout(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.iniciarComponentes();
 	}
@@ -23,9 +27,29 @@ public class Ventana extends JFrame{
 	public void iniciarComponentes()
 	{
 		JPanel login = new JPanel();
-		login.setSize(this.getWidth(), this.getHeight());
-		login.setBackground(Color.red);
+		login.setSize(this.getWidth()/2, this.getHeight());
+		login.setBackground(Color.gray);
+		login.setLayout(null);
+		
+		JLabel loginTitulo = new JLabel("ACCEDER", 0);
+		loginTitulo.setFont(new Font("Cambria", Font.BOLD, 18));
+		loginTitulo.setForeground(Color.white);
+		loginTitulo.setSize(300,80);
+		loginTitulo.setLocation(100,20);
+		loginTitulo.setOpaque(true);
+		loginTitulo.setBackground(Color.blue);
+		login.add(loginTitulo);
+		
+		JPanel registro = new JPanel(); // Agregar otro panel
+		registro.setSize(this.getWidth()/2, this.getHeight());
+		registro.setLocation(500,0);
+		registro.setBackground(Color.green);
+		
 		this.add(login);
+		this.add(registro);
+		
+		
+	
 		
 	}
 
